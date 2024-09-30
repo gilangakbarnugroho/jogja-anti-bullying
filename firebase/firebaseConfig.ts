fd55c513f23fe4f6006227e36335ed177866bb69
Gilang Akbar Nugroho <likeamanlyman@gmail.com>
2024-09-30 17:01:49 +0700

    fix env

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -5,12 +5,12 @@ import { getStorage } from 'firebase/storage';
 import { getDoc, doc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.SECRET_FIREBASE_API_KEY,
+  authDomain: process.env.SECRET_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.SECRET_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.SECRET_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.SECRET_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.SECRET_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
